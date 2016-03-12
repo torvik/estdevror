95da6bcaafa2edff839f20bf4f3ed334338025ba
steve chesnowitz <steve@chesnowitz.com>
2016-03-12 18:10:01 +0000

    nav and user form update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'pages#index'
-  get '''pages/contact'
+  get 'pages/contact'
   get 'pages/about'
 
 end
