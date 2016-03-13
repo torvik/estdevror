48fc2e0ffc5c4b6552d2f842476170efed4b7b33
steve chesnowitz <steve@chesnowitz.com>
2016-03-13 22:22:53 +0000

    added user associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ActiveRecord::Base
+  
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160312184553) do
+ActiveRecord::Schema.define(version: 20160313185110) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
