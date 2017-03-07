59bf33ce23631351005e2c35eee469f0640b76c6
torvik <pless.victor@gmail.com>
2017-03-07 23:00:45 +0000

    aws

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    :region => ENV['AWS_REGION'],
+    :region =>  "s3-#{ENV['AWS_REGION']}.amazonaws.com",
     s3_credentials: {
       bucket: ENV['S3_BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
